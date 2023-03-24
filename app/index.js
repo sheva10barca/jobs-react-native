1a8fafa15f453df92f59a8aca131132f57e5c158
sheva <vladislavshevchenko10@gmail.com>
2023-03-24 13:55:40 +0200

    here we go

---
+++ app/index.js
@@ -0,0 +1,46 @@
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from '../constants';
+import ScreenHeaderBtn from '../components/common/header/ScreenHeaderBtn';
+import Welcome from '../components/home/welcome/Welcome';
+import Popularjobs from '../components/home/popular/Popularjobs';
+import Nearbyjobs from '../components/home/nearby/Nearbyjobs';
+
+const Home = () => {
+   const router = useRouter();
+   const [searchTerm, setSearchTerm] = useState('');
+
+   return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+         <Stack.Screen
+            options={{
+               headerStyle: {
+                  backgroundColor: COLORS.lightWhite,
+               },
+               headerShadowVisible: false,
+               headerLeft: () => <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />,
+               headerRight: () => <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />,
+               headerTitle: '',
+            }}
+         />
+         <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={{ flex: 1, padding: SIZES.medium }}>
+               <Welcome
+                  searchTerm={searchTerm}
+                  setSearchTerm={setSearchTerm}
+                  handleClick={() => {
+                     if (searchTerm) {
+                        router.push(`/search//${searchTerm}`);
+                     }
+                  }}
+               />
+               <Popularjobs />
+               <Nearbyjobs />
+            </View>
+         </ScrollView>
+      </SafeAreaView>
+   );
+};
+
+export default Home;
